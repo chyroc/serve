4c506b8dfc8b27c2e28bed50cd9af9acbec116d5
chyroc <chyroc@qq.com>
2021-12-15 15:26:09 +0800

    支持跨域

--- internal/gin.go
+++ internal/gin.go
@@ -1,19 +1,32 @@
 package internal
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/gin-gonic/gin"
 )
 
-func CreateEngine(dir string) *gin.Engine {
-	for _, v := range os.Environ() {
-		fmt.Println(v)
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
 	}
+}
+
+func CreateEngine(dir string, isDisableCors bool) *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 	r.Use(gin.Logger())
+	if !isDisableCors {
+		r.Use(CORSMiddleware())
+	}
 	r.NoRoute(func(c *gin.Context) {
 		contentType, res, err := ReadPath(dir, c.Request.URL.Path)
 		if err != nil {
